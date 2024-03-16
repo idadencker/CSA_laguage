a06453a3b7acc54c1b01e18006c7ec8e56e2f5f6
idadencker <ida@dencker.net>
2024-03-16 16:41:35 +0100

    started on assignment 3

---
+++ cds-lang-assignment-3/README.md
@@ -0,0 +1,4 @@
+To make the program work
+
+run the bash script 'setup.sh' in terminal
+run the bash script 'run.sh' in terminal
---
+++ cds-lang-assignment-3/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# activate the environment
+source ./env/bin/activate
+# run the code
+python src/QueryExpansion.py
+# close the environment
+deactivate
