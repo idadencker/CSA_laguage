f600718e70418bceae7fd54c825c5f994612e3cc
idadencker <ida@dencker.net>
2024-03-21 15:02:27 +0100

    done assignment 3

--- cds-lang-assignment-3/run.sh
+++ cds-lang-assignment-3/run.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/bash
-
 # activate the environment
 source ./env/bin/activate
 # run the code
-python src/QueryExpansion.py
+python src/QueryExpansion.py "$@"
 # close the environment
 deactivate
--- cds-lang-assignment-3/setup.sh
+++ cds-lang-assignment-3/setup.sh
@@ -1,18 +1,9 @@
-#!/usr/bin/env bash
-#!/usr/bin/bash
-
 # create virtual env
 python -m venv env
 # activate env
 source ./env/bin/activate
 # install requirements
 pip install --upgrade pip
-
-
-#does the pip install pipreqs and pipreqs src —savepath requirements.txt go here???
-
-
-#python3 -m pip install -r requirements.txt 
 pip install -r requirements.txt
 # close the environment
 deactivate
